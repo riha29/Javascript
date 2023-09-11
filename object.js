4d00d07f2aecaaa368e45835bf79035825d93fca
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2023-09-11 23:47:35 +0600

    js_objects

---
+++ object.js
@@ -0,0 +1,8 @@
+var book ={
+    title: 'start with why',
+    author: 'simon sinek',
+    price: 200,
+    pages: 246
+}
+
+console.log(book.price)
---
+++ parameter.js
@@ -0,0 +1,9 @@
+price= 10
+function bringSingara(taka, quantity){
+    console.log('singarar jonno dise' ,taka)
+    console.log('mama singara den')
+
+    return taka-quantity*price
+}
+
+bringSingara(100, 5)
