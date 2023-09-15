e54160d129e4313b65fdc0195ccc4331ec8d801f
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2023-09-15 23:51:08 +0600

    js_ES6

---
+++ map.js
@@ -0,0 +1,15 @@
+const numbers= [4,6,8,10]
+
+// console.log(numbers.map(n=>n*2))
+
+const friends= ['akdum', 'bakdum','ghoradum', 'saje']
+// console.log(friends.map(friend=>friend.length))
+
+const product=[
+    {name: 'bottle', price:50, color:'yellow'},
+    {name: 'bowl', price:70, color:'yellow'},
+    {name: 'plate', price:80, color:'yellow'},
+    {name: 'glass', price:10, color:'yellow'}
+]
+
+console.log(product.map(p=>p.name))
