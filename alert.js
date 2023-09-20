7b6da8360159667b620ef256da9e64087622851c
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2023-09-20 20:29:59 +0600

    js_alert_confirm

---
+++ alert.js
@@ -0,0 +1,22 @@
+console.log('i am here!')
+
+
+// contains only okay
+const watching= ()=> {
+    alert('open the book!')
+}
+
+// constains okay or cancel
+const askPicnic= ()=> {
+    confirm('picnic e jaba to??')
+}
+
+// okay denotes true and cancel denotes false if
+// we console log th response of confirm
+
+const askName= ()=> {
+    const name= prompt('Your name?')
+    if(name){
+        console.log(name)
+    }
+}
---
+++ explore.js
@@ -0,0 +1,7 @@
+const name= 15
+const student= {name: 'jolil', friend:'borsha', job:'business', makeMovie: function (name) {
+    console.log(name)
+}}
+
+student.makeMovie('Din the day')
+document.getElementById('name')
